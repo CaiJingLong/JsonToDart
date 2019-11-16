fc1b29ef27962e2238d30cae1b8bca10779343f3
cjl_macbook <cjl_spy@163.com>
2019-11-16 11:09:21 +0800

    Update mini version to 10.14.

--- Flutter/desktop/go/cmd/compatibility_darwin.go
+++ Flutter/desktop/go/cmd/compatibility_darwin.go
@@ -2,7 +2,7 @@ package main
 
 // the file compatibility_darwin.go is used to set custom compile fags
  
-// #cgo darwin CFLAGS: -mmacosx-version-min=10.13
-// #cgo darwin LDFLAGS: -mmacosx-version-min=10.13
-// #cgo darwin CXXFLAGS: -mmacosx-version-min=10.13
+// #cgo darwin CFLAGS: -mmacosx-version-min=10.14
+// #cgo darwin LDFLAGS: -mmacosx-version-min=10.14
+// #cgo darwin CXXFLAGS: -mmacosx-version-min=10.14
 import "C"
