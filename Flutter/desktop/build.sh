9d5dfe10b10ade5dbc8220cc70653eb9e6120645
cjl_macbook <cjl_spy@163.com>
2019-11-16 11:37:49 +0800

    Update build shell.

--- Flutter/desktop/build.sh
+++ Flutter/desktop/build.sh
@@ -1,5 +1,6 @@
 flutter pub get
 flutter config --enable-macos-desktop
 go get -u github.com/go-flutter-desktop/hover
-~/go/bin/hover build darwin
+~/go/bin/hover build darwin-bundle
 ls go/build/outputs/darwin
+ls go/build/outputs/darwin-bundle
